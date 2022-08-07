1e4227f41488acd746cccb62db508a1ba9845e92
Arjun GS <arjungsanal@gmail.com>
2022-08-07 22:57:38 +0530

    Landing Section Completed

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,9 +5,9 @@ import logo from "../images/logo.png";
 import { useState } from "react";
 
 const NavBarItem = ({ title, classprops }) => (
-  <li className={`mx-4 cursor-pointer ${classprops}`}>
+  <a target='_blank' rel="noopener noreferrer " href="https://github.com/arjungsanal"><li className={`mx-4 cursor-pointer ${classprops}`}>
     {title}
-  </li>
+  </li></a>
 );
 
 
@@ -16,37 +16,38 @@ const Navbar = () => {
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4 py-3">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
-        <img src={logo} alt="Logo" className="w-32 cursor-pointer" />
+        <a href="./"><img src={logo} alt="Logo" className="w-32 cursor-pointer" /> </a>
       </div>
       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
-   {["Market" , "Exchange" , "Wallet" , "About Dev"].map((item , index )=>(
-    <NavBarItem key={item +index} title={item} />
-   ))}
+        {["Market", "Exchange", "Wallet", "About Dev"].map((item, index) => (
+          <NavBarItem key={item + index} title={item} />
+          
+        ))}
 
-   <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
-    Login
-   </li>
+        <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+          Login
+        </li>
 
       </ul>
 
       <div className="flex relative">
         {
           toggleMenu
-          ?
-          <AiOutlineClose fontSize={28} className='text-white md:hidden cursor-pointer '  onClick={()=>setToggleMenu(false)}/>
-          :
-          <HiMenuAlt4  fontSize={28} className='text-white md:hidden cursor-pointer '  onClick={()=>setToggleMenu(true)}/>
+            ?
+            <AiOutlineClose fontSize={28} className='text-white md:hidden cursor-pointer ' onClick={() => setToggleMenu(false)} />
+            :
+            <HiMenuAlt4 fontSize={28} className='text-white md:hidden cursor-pointer ' onClick={() => setToggleMenu(true)} />
         }
         {
           toggleMenu && (
-            <ul  className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+            <ul className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
             flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in">
               <li className="text-xl w-full my-2">
-              <AiOutlineClose onClick={()=>setToggleMenu(false)} />
+                <AiOutlineClose onClick={() => setToggleMenu(false)} />
               </li>
-              {["Market", "Exchange",  "Wallets" , "About Dev"].map(
-              (item, index) => <NavBarItem key={item + index} title={item} classprops="my-2 text-lg" />,
-            )}
+              {["Market", "Exchange", "Wallets", "About Dev"].map(
+                (item, index) => <NavBarItem key={item + index} title={item} classprops="my-2 text-lg" />,
+              )}
             </ul>
           )
         }
